f46cbb97affac915e484f5ea63e6e7e228d414f5
Liam Middleton <118669186+liammid27@users.noreply.github.com>
2025-12-19 10:47:58 +0200

    Added editor options and gradient and text widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:dice_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 40, 117, 250),
+            Color.fromARGB(255, 119, 169, 255),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
